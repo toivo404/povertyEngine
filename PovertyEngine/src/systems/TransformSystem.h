271a9e4922cb35d15fc1c7851035e2aa3181a255
Toivo <jarvitoivo@gmail.com>
2025-01-19 17:08:57 +0200

    Put model matrix updating logic in the Transform struct itself

--- PovertyEngine/src/systems/TransformSystem.h
+++ PovertyEngine/src/systems/TransformSystem.h
@@ -1,10 +1,20 @@
 #pragma once
 #include <glm.hpp>
-
+#include <gtc/matrix_transform.hpp> // For glm::translate, glm::rotate, glm::scale
 
 struct Transform {
     glm::vec3 position = glm::vec3(0.0f);  
     glm::vec3 scale = glm::vec3(1.0f);     
-    glm::vec3 rotation = glm::vec3(0.0f);  
-    glm::mat4 model = glm::mat4(1.0f);     
+    glm::vec3 rotation = glm::vec3(0.0f);
+    glm::mat4 model = glm::mat4(1.0f);
+
+    void UpdateModelMatrix()
+    {
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, position);
+        model = glm::rotate(model, glm::radians(rotation.x), glm::vec3(1.0f, 0.0f, 0.0f));
+        model = glm::rotate(model, glm::radians(rotation.y), glm::vec3(0.0f, 1.0f, 0.0f));
+        model = glm::rotate(model, glm::radians(rotation.z), glm::vec3(0.0f, 0.0f, 1.0f));
+        model = glm::scale(model, scale);
+    }
 };
