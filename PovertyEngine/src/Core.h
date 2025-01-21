421b80b94a56c0c8904ffe8bd25b556fcffab4e3
Toivo <jarvitoivo@gmail.com>
2025-01-21 02:02:01 +0200

    Added method to combine values in to string easily

--- PovertyEngine/src/Core.h
+++ PovertyEngine/src/Core.h
@@ -4,3 +4,15 @@
 #else
 	#define PE_API __declspec(dllimport)
 #endif
+#include <sstream>
+#include <string>
+
+// Variadic template function
+template <typename... Args>
+std::string Combine(const Args&... args) {
+	std::ostringstream oss;
+	// Using a fold expression to append each argument to the stream
+	((oss << args), ...);
+	return oss.str();
+}
+
